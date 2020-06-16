8f33a11dcc530112f06285eb4698e4a8c4633996
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-06-16 13:43:38 +0900

    localdb接続とviewを作成及びデータバインディングの確認

---
+++ Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Summary{ get; set; }
+        public string Detail{ get; set; }
+        public DateTime Limit{ get; set; }
+        public bool Done { get; set; }
+    }
+}
