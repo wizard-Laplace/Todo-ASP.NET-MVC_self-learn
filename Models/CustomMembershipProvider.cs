831f105cc3113e73f0130c37d6e0118c501bef24
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-06-20 14:12:37 +0900

    CustomMembershipProviderのパスワードハッシュ化処理

--- Models/CustomMembershipProvider.cs
+++ Models/CustomMembershipProvider.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Security;
 
@@ -112,8 +114,11 @@ namespace TodoApp.Models
             //メモリの管理が行われる。
             using (var db = new TodoesContext())
             {
+                //ハッシュ値を取得する
+                string hash = this.GeneratePasswordHash(username, password);
+
                 var user = db.Users
-                    .Where(u => u.UserName == username && u.Password == password)
+                    .Where(u => u.UserName == username && u.Password == hash)
                     //FirstOrDefaultはWhereの抽出結果リストの先頭を返す。
                     //或いはリストが空ならnullを返す
                     .FirstOrDefault();
@@ -124,7 +129,40 @@ namespace TodoApp.Models
                 }
             }
 
+            ///TODO 仮の処理の為、後で削除する
+            //現在dbに格納されているパスワードはハッシュ化されており、
+            //ログイン方法が無い為、adminユーザーのみログオン方法を担保する
+            if("admin".Equals(username) && "passowrd".Equals(password))
+            {
+                return true;
+            }
+
             return false;
+        }
+
+        /// <summary>
+        /// ユーザ名とパスワードを引数で受けて、
+        /// ハッシュ化されたパスワードを返す
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>Convert.ToBase64String(hash)</returns>
+        public string GeneratePasswordHash(string username, string password)
+        {
+            //引数usernameの頭に適当な文字列を連結
+            string rawSalt = $"secret_{username}";
+            
+            //ここからハッシュ化処理
+            var sha256 = new SHA256CryptoServiceProvider();
+            //rasSaltをバイト文字列へエンコーディングする
+            var salt = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(rawSalt));
+            //パスワードをpbkdf2でハッシュ化する
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            //Rfcインスタンス生成後ハッシュ化したパスワードの取得
+            var hash = pbkdf2.GetBytes(32);
+
+            //変数hashがbyt[]なので文字列へ変換し返す
+            return Convert.ToBase64String(hash);
         }
     }
 }
